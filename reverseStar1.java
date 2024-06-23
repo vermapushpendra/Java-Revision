af01bc27b3976738b40c51bf77d7982f862996d7
Pushpendra verma <pv842984@gmail.com>
2024-06-23 16:17:05 +0530

    star patterns added

---
+++ reverseStar1.java
@@ -0,0 +1,13 @@
+public class reverseStar1 {
+    public static void main(String[] args) {
+
+        int n = 4;
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = i; j >= 1; j--) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
